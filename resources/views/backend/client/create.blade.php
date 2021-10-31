b09e0190b1c613798ab76475dceff697a3e3b5b8
azmeer <azmeer.sc@gmail.com>
2021-10-31 09:36:44 +0530

    Client type selector added

--- resources/views/backend/client/create.blade.php
+++ resources/views/backend/client/create.blade.php
@@ -1,6 +1,24 @@
 @extends('backend.layouts.app')
 @section('content')
 
+@push('after-scripts')
+<script>
+    $(document).ready(function(e) {
+        $('#client_info_company').hide();
+        $('#client_info_person').hide();
+    });
+    function select_type(client_type){
+        console.log(client_type);
+        if(client_type=="Individual"){
+            $('#client_info_company').hide();
+            $('#client_info_person').show();
+        }else{
+            $('#client_info_company').show();
+            $('#client_info_person').show();
+        }
+    }
+</script>
+@endpush
 
 <x-forms.post :action="route('admin.client.store')" class="was-validated" id="myForm">
 
@@ -27,52 +45,72 @@
         </section>
         <!--card-header-->
 
-        <section class="card-body">
-            <p>A Client can be an individual or a company. Please include a primary contact for a company.</p>
-            
-            <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label text-right">Company name</label>
+        <main class="card-body">
+            <p>A Client can be an individual or a company. In case for a company, please include a primary contact.</p>
 
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="company_name" name="company_name"
-                        placeholder="Company Name" required value="{{old('company_name')}}" />
-                    @error('company_name') <span class="text-danger error">{{ $message }}</span>@enderror
+            <aside class="alert alert-dark">
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" name="client_type" id="client_type1" value="Individual"
+                        class="custom-control-input" onclick="select_type(value)">
+                    <label class="custom-control-label" for="client_type1">Individual</label>
                 </div>
-            </div>
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" name="client_type" id="client_type2" value="Company"
+                        class="custom-control-input" onclick="select_type(value)">
+                    <label class="custom-control-label" for="client_type2">Company</label>
+                </div>
+            </aside>
 
-            <div class="form-group row">
-                <label for="contact" class="col-sm-2 col-form-label text-right">Contact</label>
-                <div class="col-sm-5">
-                    <input type="text" class="form-control" pattern="[1-9]\d+" minlength="11" maxlength="15" id="phone"
-                        name="company_phone" placeholder="Phone" required value="{{old('company_phone')}}" />
-                    @error('company_phone') <span class="text-danger error">{{ $message }}</span>@enderror
-                </div>
-                <div class="col-sm-5">
-                    <input type="text" class="form-control" id="company_email" name="company_email" placeholder="Email"
-                        required value="{{old('company_email')}}" />
-                    @error('company_email') <span class="text-danger error">{{ $message }}</span>@enderror
-                </div>
-            </div>
+            <section id="client_info_company">
+                <h4>Company information</h4>
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label text-right">Company name</label>
 
-            <hr/>
-            <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label text-right">Name</label>
-                <div class="col-sm-2">
-                    <select class="form-control" name='title' id="title" required>
-                        <option value="">--Title--</option>
-                        @foreach( App\Models\UserExtra::getEnum('Titles') as $value)
-                            <option value="{{ $value }}" {{ old('title') == $value?'selected':''}}>{{ $value }}</option>
-                        @endforeach
-                    </select>
-                    @error('title') <span class="text-danger error">{{ $message }}</span>@enderror
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="company_name" name="company_name"
+                            placeholder="Company Name" required value="{{old('company_name')}}" />
+                        @error('company_name') <span class="text-danger error">{{ $message }}</span>@enderror
+                    </div>
                 </div>
-                <div class="col-sm-8">
-                    <input type="text" class="form-control" id="name" name="name" placeholder="Name"
-                        required value="{{old('name')}}" />
-                    @error('name') <span class="text-danger error">{{ $message }}</span>@enderror
+
+                <div class="form-group row">
+                    <label for="contact" class="col-sm-2 col-form-label text-right">Contact</label>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" pattern="[1-9]\d+" minlength="11" maxlength="15"
+                            id="phone" name="company_phone" placeholder="Phone" required
+                            value="{{old('company_phone')}}" />
+                        @error('company_phone') <span class="text-danger error">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" id="company_email" name="company_email"
+                            placeholder="Email" required value="{{old('company_email')}}" />
+                        @error('company_email') <span class="text-danger error">{{ $message }}</span>@enderror
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+
+            <section id="client_info_person">
+                <hr />
+                <h4>Primary contact</h4>
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label text-right">Name</label>
+                    <div class="col-sm-2">
+                        <select class="form-control" name='title' id="title" required>
+                            <option value="">--Title--</option>
+                            @foreach( App\Models\UserExtra::getEnum('Titles') as $value)
+                            <option value="{{ $value }}" {{ old('title')==$value?'selected':''}}>{{ $value }}</option>
+                            @endforeach
+                        </select>
+                        @error('title') <span class="text-danger error">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Name" required
+                            value="{{old('name')}}" />
+                        @error('name') <span class="text-danger error">{{ $message }}</span>@enderror
+                    </div>
+                </div>
+            </section>
+        </main>
         <!--card-body-->
 
         <section class="card-footer">
